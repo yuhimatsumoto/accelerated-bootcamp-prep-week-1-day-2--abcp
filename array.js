3eaaace205431bd8945d4ab8fd1cc4a48cb26ba7
Yuhi Matsumoto <ym.starbuckslover@icloud.com>
2018-01-14 17:45:15 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -1,5 +1,6 @@
 function addElementToBeginningOfArray(array, element) {
  // add the element to the beginning of the array without mutating the array
+ return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
