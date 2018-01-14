becd35ec9b720907dbc72bb1fbf0f35060626459
Yuhi Matsumoto <ym.starbuckslover@icloud.com>
2018-01-14 17:50:16 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -14,7 +14,8 @@ return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+array.push(element)
+return array
 }
 
 function accessElementInArray(array, index) {
