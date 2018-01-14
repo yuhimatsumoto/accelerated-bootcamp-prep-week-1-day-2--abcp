a037b92c6b21ec86676373b7b8c5d2697b7606c3
Yuhi Matsumoto <ym.starbuckslover@icloud.com>
2018-01-14 18:13:22 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -23,7 +23,8 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-
+array.shift(element)
+return array
 }
 
 function removeElementFromBeginningOfArray(array) {
